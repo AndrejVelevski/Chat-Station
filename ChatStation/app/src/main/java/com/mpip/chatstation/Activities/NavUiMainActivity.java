95535106d5e92b22d97a6969b9525cdba823bc2d
mVucevski <placeformovies@gmail.com>
2019-07-13 02:04:51 +0200

    Add Fragments

--- ChatStation/app/src/main/java/com/mpip/chatstation/Activities/NavUiMainActivity.java
+++ ChatStation/app/src/main/java/com/mpip/chatstation/Activities/NavUiMainActivity.java
@@ -1,6 +1,7 @@
 package com.mpip.chatstation.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.graphics.Color;
 import android.os.Bundle;
@@ -9,13 +10,15 @@ import android.widget.Toast;
 
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
+import com.mpip.chatstation.Fragments.FriendsFragment;
+import com.mpip.chatstation.Fragments.HomeFragment;
+import com.mpip.chatstation.Fragments.MessagesFragment;
 import com.mpip.chatstation.R;
 
 public class NavUiMainActivity extends AppCompatActivity {
 
     AHBottomNavigation bottomNavigation;
-    int count = 0;
-    Button btn_count = findViewById(R.id.btn_count);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,18 +37,39 @@ public class NavUiMainActivity extends AppCompatActivity {
         bottomNavigation.addItem(item3);
 
         // Set background color
-        bottomNavigation.setDefaultBackgroundColor(Color.parseColor("#009688"));
+        //bottomNavigation.setDefaultBackgroundColor(Color.parseColor("#009688"));
 
         // Use colored navigation with circle reveal effect
-        bottomNavigation.setColored(true);
+        //bottomNavigation.setColored(true);
 
-        btn_count.setOnClickListener(v->{
-            count++;
-            bottomNavigation.setNotification(""+count,1);
-        });
+
+
+        //poceten fragment
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment(bottomNavigation)).commit();
 
         bottomNavigation.setOnTabSelectedListener((pos,isSelected)->{
             Toast.makeText(NavUiMainActivity.this, "Start activity: " + pos, Toast.LENGTH_SHORT).show();
+
+            Fragment selectedFragment = null;
+
+            switch (pos){
+                case 0:
+                    selectedFragment = new HomeFragment(bottomNavigation);
+                    break;
+                case 1:
+                    selectedFragment = new MessagesFragment();
+                    break;
+                case 2:
+                    selectedFragment = new FriendsFragment();
+                    break;
+            }
+
+            if(selectedFragment!=null){
+                getSupportFragmentManager().beginTransaction().add(R.id.fragment_container,selectedFragment)
+                        .commit();
+            }
+
             return true;
         });
     }
