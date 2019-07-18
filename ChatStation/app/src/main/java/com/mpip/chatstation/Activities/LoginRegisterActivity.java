4509cc69387270a812e73b32d7f2ca08e8ff3976
mVucevski <placeformovies@gmail.com>
2019-07-18 02:06:06 +0200

    Implement Login/Register UI & Custom Toast

---
+++ ChatStation/app/src/main/java/com/mpip/chatstation/Activities/LoginRegisterActivity.java
@@ -0,0 +1,66 @@
+package com.mpip.chatstation.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.mpip.chatstation.Config.Constants;
+import com.mpip.chatstation.Fragments.LoginFragment;
+import com.mpip.chatstation.R;
+
+public class LoginRegisterActivity extends AppCompatActivity {
+    private FragmentManager fragmentManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_register);
+
+        fragmentManager = getSupportFragmentManager();
+
+        // If savedinstnacestate is null then replace login fragment
+        if (savedInstanceState == null) {
+            fragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frameContainer, new LoginFragment(),
+                            Constants.Login_Fragment).commit();
+        }
+
+        // On close icon click finish activity
+        findViewById(R.id.close_activity).setOnClickListener(
+                (v) -> finish());
+    }
+
+    // Replace Login Fragment with animation
+    public void replaceLoginFragment() {
+        fragmentManager
+                .beginTransaction()
+                .setCustomAnimations(R.anim.left_enter, R.anim.right_exit)
+                .replace(R.id.frameContainer, new LoginFragment(),
+                        Constants.Login_Fragment).commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        // Find the tag of signup and forgot password fragment
+        Fragment SignUp_Fragment = fragmentManager
+                .findFragmentByTag(Constants.SignUp_Fragment);
+        Fragment ForgotPassword_Fragment = fragmentManager
+                .findFragmentByTag(Constants.ForgotPassword_Fragment);
+
+        // Check if both are null or not
+        // If both are not null then replace login fragment else do backpressed
+        // task
+
+        if (SignUp_Fragment != null)
+            replaceLoginFragment();
+        else if (ForgotPassword_Fragment != null)
+            replaceLoginFragment();
+        else
+            super.onBackPressed();
+    }
+}
--- ChatStation/app/src/main/java/com/mpip/chatstation/Config/Constants.java
+++ ChatStation/app/src/main/java/com/mpip/chatstation/Config/Constants.java
@@ -6,4 +6,9 @@ public class Constants
 
     public static final String EMAIL = "email";
     public static final String MESSAGE = "message";
+
+    //Fragments Tags
+    public static final String Login_Fragment = "Login_Fragment";
+    public static final String SignUp_Fragment = "SignUp_Fragment";
+    public static final String ForgotPassword_Fragment = "ForgotPassword_Fragment";
 }
