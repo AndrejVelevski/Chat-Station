23bb81f780eac597866bc34a105358048a0236ab
mVucevski <placeformovies@gmail.com>
2019-09-14 18:35:35 +0200

    link users info

--- ChatStation/app/src/main/java/com/mpip/chatstation/Activities/UserDetailsActivity.java
+++ ChatStation/app/src/main/java/com/mpip/chatstation/Activities/UserDetailsActivity.java
@@ -3,6 +3,7 @@ package com.mpip.chatstation.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.mpip.chatstation.Config.Constants;
 import com.mpip.chatstation.Models.User;
@@ -11,6 +12,10 @@ import com.mpip.chatstation.R;
 
 public class UserDetailsActivity extends AppCompatActivity
 {
+    private TextView tvUsername;
+    private TextView tvFirstName;
+    private TextView tvLastName;
+    private TextView tvAge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -22,5 +27,19 @@ public class UserDetailsActivity extends AppCompatActivity
         User user = (User) getIntent().getSerializableExtra(Constants.USER);
 
         System.out.println(user.email);
+
+        initUI();
+
+        tvUsername.setText(user.username);
+        tvFirstName.setText(user.first_name);
+        tvLastName.setText(user.last_name);
+        tvAge.setText(user.age);
+    }
+
+    private void initUI(){
+        tvUsername = findViewById(R.id.userInfoUsername);
+        tvFirstName = findViewById(R.id.userInfoFirstName);
+        tvLastName = findViewById(R.id.userInfoLastName);
+        tvAge = findViewById(R.id.userInfoAge);
     }
 }
